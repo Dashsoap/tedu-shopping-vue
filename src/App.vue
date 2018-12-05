98c6f07cd0ffc982de93d83d653953cd4a77ae65
wsq <mywsq961@gmail.com>
2018-12-05 16:44:21 +0800

    增加全局样式

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,27 @@
 <template>
-    <div id="app">
-        <my-header class="header"></my-header>
-        <router-view class="my-body"></router-view>
-    </div>
+  <div id="app">
+   <my-header class="header"></my-header>
+   <router-view class="my-body"></router-view>
+  </div>
 </template>
 
 <script>
-import MyHeader from "./components/Header/Header";
+import MyHeader from './components/Header/Header'
 
 export default {
-    name: "app",
-    components: {
-        MyHeader
-    }
-};
+  name: 'app',
+  components: {
+    MyHeader
+  }
+}
 </script>
 
-<style lang="stylus">
-@import '/assets/theme.styl';
-
-html {
-    box-sizing: border-box;
-    font-size: 16px;
+<style>
+html{
+  box-sizing: border-box;
+  font-size: 16px;
 }
-
-body {
-    margin: 0;
-}
-
-a {
-    text-decoration: none;
-    color: $primary;
-    font-weight: 500;
-}
-
-.mt-1 {
-    margin-top: 10px;
-}
-
-.mt-2 {
-    margin-top: 20px;
-}
-
-.mt-3 {
-    margin-top: 30px;
+body{
+  margin: 0;
 }
 </style>
