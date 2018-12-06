8e3245e4a8dffc57a9382dc1d6144c84c424129c
wsq <mywsq961@gmail.com>
2018-12-06 11:00:47 +0800

    代码风格修正

--- src/main.js
+++ src/main.js
@@ -2,37 +2,32 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import VueRouter from 'vue-router';
-import {
-  library
-} from '@fortawesome/fontawesome-svg-core';
-import {
-  faSearch,
-  faShoppingCart
-} from '@fortawesome/free-solid-svg-icons'
-import ElementUI from 'element-ui'
-import './theme/index.css'
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
-import VueProgressBar from 'vue-progressbar'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import ElementUI from 'element-ui';
+import './theme/index.css';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import VueProgressBar from 'vue-progressbar';
 
-library.add(faSearch,faShoppingCart);
-Vue.config.productionTip = false
+library.add(faSearch, faShoppingCart);
+Vue.config.productionTip = false;
 
 const options = {
-  color: '#EA6F5A',
-  transition: {
-    speed: '0.3s',
-    opacity: '1s',
-    termination: 400
-  },
-  autoRevert: true,
-  inverse: false
-}
+	color: '#EA6F5A',
+	transition: {
+		speed: '0.3s',
+		opacity: '1s',
+		termination: 400,
+	},
+	autoRevert: true,
+	inverse: false,
+};
 
-Vue.use(VueProgressBar, options)
-Vue.use(ElementUI)
+Vue.use(VueProgressBar, options);
+Vue.use(ElementUI);
 Vue.use(VueRouter);
 Vue.component('fa-icon', FontAwesomeIcon);
 new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+	router,
+	render: (h) => h(App),
+}).$mount('#app');
